66a5ef43b0a805a9770d144f18327bc7c678cec4
Jonathan Steven Sorto Rosales <rosalesjonathan2024@gmail.com>
2024-07-23 17:37:31 -0600

    sumar2numeros

---
+++ MIPRIMERPROYECTO/MIPRIMERPROYECTO/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MIPRIMERPROYECTO
+{
+    class Program
+    { 
+        static void Main(string[] args)
+        {
+            Console.Write("num 1:");
+            int num1 = int.Parse(Console.ReadLine()); //"5" -> 5
+
+            Console.Write("num 2:");
+            int num2 = int.Parse(Console.ReadLine());
+
+            int respuesta = num1 + num2;
+            Console.Write("La sumas de {0} + {1} ={2}", num1, num2, respuesta);
+
+            //pausa
+            Console.ReadLine();
+        }
+    }
+}
