0a61d5505646e938845a748e68fb2e0a7d90ceac
Jonathan Steven Sorto Rosales <rosalesjonathan2024@gmail.com>
2024-07-23 21:31:42 -0600

    nota de los 3 computos yeah

--- Obtener nota final program1/Obtener nota final program1/Program.cs
+++ Obtener nota final program1/Obtener nota final program1/Program.cs
@@ -10,18 +10,51 @@ namespace Obtener_nota_final_program1
     {
         static void Main(string[] args)
         {
-            //Obtener Nota Final Computo 1 Program 1
-            Console.Write("Lab1: ");
-            double lab1 = double.Parse(Console.ReadLine()); // 9
+            //Obtener Nota Final Computo 1 Programa 1
+            Console.Write("Lab 1 Computo 1: ");
+            double lab1_c1 = double.Parse(Console.ReadLine());
 
-            Console.Write("Lab2: ");
-            double lab2 = double.Parse(Console.ReadLine()); //7
+            Console.Write("Lab 2 Computo 1: ");
+            double lab2_c1 = double.Parse(Console.ReadLine());
 
-            Console.Write("Parcial 1: ");
-            double parcial1 = double.Parse(Console.ReadLine()); //9
+            Console.Write("Parcial 1 Computo 1: ");
+            double parcial1_c1 = double.Parse(Console.ReadLine());
 
-            double c1 = lab1 * 30 / 100 + lab2 * 30 / 100 + parcial1 * 40 / 100;
-            Console.WriteLine("La nota de c1 es: {0}", c1);
+            //Computo 1
+            double c1 = lab1_c1 * 30 / 100 + lab2_c1 * 30 / 100 + parcial1_c1 * 40 / 100;
+            Console.WriteLine("La nota total del Computo 1: {0:F2}", c1);
+
+            Console.WriteLine();
+
+            //Obtener Nota Final Computo 2 Programa 1
+            Console.Write("Lab 1 Computo 2: ");
+            double lab1_c2 = double.Parse(Console.ReadLine());
+
+            Console.Write("Lab 2 Computo 2: ");
+            double lab2_c2 = double.Parse(Console.ReadLine());
+
+            Console.Write("Parcial 1 Computo 2: ");
+            double parcial1_c2 = double.Parse(Console.ReadLine());
+
+            //Computo 2
+            double c2 = lab1_c2 * 30 / 100 + lab2_c2 * 30 / 100 + parcial1_c2 * 40 / 100;
+            Console.WriteLine("La nota total del Computo 2: {0:F2}", c2);
+
+            Console.WriteLine();
+
+            //Obtener Nota Final Computo 3 Programa 1
+            Console.Write("Lab 1 Computo 3: ");
+            double lab1_c3 = double.Parse(Console.ReadLine());
+
+            Console.Write("Lab 2 Computo 3: ");
+            double lab2_c3 = double.Parse(Console.ReadLine());
+
+            Console.Write("Parcial 1 Computo 3: ");
+            double parcial1_c3 = double.Parse(Console.ReadLine());
+
+            //Computo 3
+            double c3 = lab1_c3 * 30 / 100 + lab2_c3 * 30 / 100 + parcial1_c3 * 40 / 100;
+            Console.WriteLine("La nota total del Computo 3: {0:F2}", c3);
 
             //pausa
             Console.ReadLine();
