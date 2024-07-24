99d6bedcad7996438f953865a9984196e9a37687
Jonathan Steven Sorto Rosales <rosalesjonathan2024@gmail.com>
2024-07-23 20:38:03 -0600

    Operador logico

---
+++ operador logico/operador logico/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace operador_logico
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Operadores logicos ejemplo
+            int respuesta = 5 + 8 - 2 * 5 / 4 + 1; //12.00
+            Console.WriteLine("La respuesta es: {0})",respuesta);
+
+            //pausa
+            Console.ReadLine();
+        }
+    }
+}
